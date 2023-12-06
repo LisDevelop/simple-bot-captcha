7b3612e6ad8ff581deaa25a17bb03607116b7d79
Julio Cesar <144570106+LisDevelop@users.noreply.github.com>
2023-12-05 21:19:03 -0300

    update new configurations

--- modules/events/interactionCreate/checking-captcha.js
+++ modules/events/interactionCreate/checking-captcha.js
@@ -1,6 +1,8 @@
 const low = require('lowdb')
 const path = require('path');
 const FileSync = require('lowdb/adapters/FileSync');
+const Canvas = require('canvas');
+const { EmbedBuilder } = require('@discordjs/builders');
 
 const check_BOT = new FileSync(path.resolve(__dirname, '../../configurations/configsBOT.json'));
 const config_BOT = low(check_BOT)
@@ -12,26 +14,31 @@ const loadImage = async () => {
     const background = await Canvas.loadImage(path.resolve(__dirname, "../../configurations/assets/background.png"));
     return background;
 };
+
+function generateCharacters (count = 20) { // Atualização automática de criação de senha
+    let password = ''
+    while (password.length < count) {
+      password += Math.random().toString(36).substr(2)
+    }
+    return password.substr(0, count)
+  }
+
 const generateImage = async (interaction, client) => {
     config_BOT.read(); config_BOT.write()
-    
+
+    const changeInfo = config_BOT.get('CONFIG_DO_BOT').find({bot_configurations: 'NO_MODIFY'}).value()
+
     Canvas.registerFont(path.resolve(__dirname, '../../configurations/assets/Highman.ttf'), { family: 'Highman Trial' })
-    let selected = interaction.values[0];
-    const hoje = moment();
-    const timeHJ = hoje.format('DD/MM');
 
-    const dataResultado = hoje.add(selected, 'days');
-    const dataFormatada = dataResultado.format('DD/MM');
-
-    const canvas = Canvas.createCanvas(1772, 2480);
+    const code = generateCharacters(5);
+    const canvas = Canvas.createCanvas(11, 3);
     const context = canvas.getContext('2d');
   
     const background = await loadImage();
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    const texto1 = `${timeHJ}`;
-    const text2 = `${dataFormatada}`
-    const tamanhoFonte1 = 162.5;
-    const corTexto1 = '#CA2D2D';
+    const texto1 = `${code}`;
+    const tamanhoFonte1 = 58;
+    const corTexto1 = '#FFFFFF';
     const fonte1 = 'Highman Trial';
 
     // Configura as propriedades do texto
@@ -43,12 +50,19 @@ const generateImage = async (interaction, client) => {
     const textoY = 2037.5;
     context.fillText(texto1, textoX, textoY);
 
-    // Posiciona o texto no canvas
-    const textoX2 = 1268.5;
-    const textoY2 = 2037.5;
-    context.fillText(text2, textoX2, textoY2);
-
     const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), 'profile-image.jpg');
+
+    // sys
+    const changeUser = client.guild.cache.get(changeInfo.bot_idGuilda).members.cache.find(u => u.id === `${interaction.user.id}`);
+    changeUser.send({content: "> **BEM-VINDO(A)!!!**\n- Para verificar o nosso captcha é bem simples e prático, para isso, você deve apenas digitalizar os caracteres que estão na imagem. Para fazer isso, clique no botão logo abaixo e coloque o código que está.", files: [attachment]}).then(msg => {
+        config_USERS.get('users').push({
+            userid: `${interaction.user.id}`,
+            username: `${interaction.user.username}`,
+            code: `${code}`
+        }).write()
+    }).catch(err => {
+        interaction.reply({content: `**[AVISO]:** Tive um erro ao enviar uma mensagem no seu privado. Libere antes de solicitar o captcha! Caso ainda tenha problemas, envie uma mensagem para: markusfiller`, ephemeral: true})
+    })
 };
 
 module.exports = (interaction) => {
@@ -59,9 +73,9 @@ module.exports = (interaction) => {
         // verificações
         const checkUsers = config_USERS.get('users').find({userid: `${interaction.user.id}`}).value()
         if(checkUsers === undefined){
-
+            generateImage(interaction, client);
         }else{
-            interaction.reply({content: "**[AVISO]** | Voc~e já possui um captcha aberto no nosso privado! Vá lá e de uma olhada."})
+            interaction.reply({content: "**[AVISO]** | Voc~e já possui um captcha aberto no nosso privado! Vá lá e de uma olhada.", ephemeral: true})
         }
     }
 }

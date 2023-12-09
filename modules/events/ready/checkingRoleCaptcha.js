a63c401a64ef2f8aec1f9fc016ad5f2b4a997195
Julio Cesar <144570106+LisDevelop@users.noreply.github.com>
2023-12-08 21:09:51 -0300

    update complete

--- modules/events/ready/checkingRoleCaptcha.js
+++ modules/events/ready/checkingRoleCaptcha.js
@@ -15,7 +15,7 @@ module.exports = (client) => {
         client.guilds.cache.get(check.bot_idGuilda).roles.cache.some(r => r.id === check.bot_ruleNotVerify)
         console.log('[AVISO]: Cargo verificado com sucesso!')
     } catch(err){
-        console.log('[ERRO DEPENDÊNCIA]: Você não configurou corretamente o ID da guilda ou o ID do cargo que será setado como Não verificado! Vá nas configurações e modifique.')
+        console.log('[ERRO DEPENDÊNCIA]: Você não configurou corretamente o ID da guilda do servidor principal ou o ID do cargo que será setado como Não verificado! Vá nas configurações e modifique.')
         process.exit()
     }
 }

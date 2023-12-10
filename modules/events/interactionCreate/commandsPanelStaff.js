4a93a50e69b1668a99ec9e3a3db3664a71ce9f54
Julio Cesar <144570106+LisDevelop@users.noreply.github.com>
2023-12-10 13:33:57 -0300

    Update commandsPanelStaff.js

--- modules/events/interactionCreate/commandsPanelStaff.js
+++ modules/events/interactionCreate/commandsPanelStaff.js
@@ -1,12 +1,12 @@
 const low = require('lowdb')
 const path = require('path');
 const FileSync = require('lowdb/adapters/FileSync');
-
+/*
 const check_BOT = new FileSync(path.resolve(__dirname, ''));
 const config_BOT = low(check_BOT)
-
+*/
 module.exports = (interaction) => {
-    if(interaction.customId === ''){
+    if(interaction.customId === 'checking_Panel'){
         
     }
 }

97c44c41ca72a30a41d14f730e28eb30bfbde6fc
Julio Cesar <144570106+LisDevelop@users.noreply.github.com>
2023-12-14 18:18:47 -0300

    Create updateAdmin.js

---
+++ modules/functions/updateAdmin.js
@@ -0,0 +1,22 @@
+const low = require('lowdb')
+const path = require('path');
+const FileSync = require('lowdb/adapters/FileSync');
+const { EmbedBuilder } = require('discord.js');
+
+const check_BOT = new FileSync(path.resolve(__dirname, '../configurations/administrators.json'));
+const config_BOT = low(check_BOT)
+
+function updateAdmin(selectUser){
+    config_BOT.read(); config_BOT.write()
+
+    const infouser = config_BOT.get('admins').find({userID: `${selectUser}`}).value()
+
+    let embed = new EmbedBuilder()
+    .setTitle('Quadro dos administradores')
+    .setThumbnail('https://media.discordapp.net/attachments/1077814640513384448/1182491145763295333/praia_grande_rp.png')
+    .setTimestamp()
+    .setFooter({text: 'Atualizado em: '})
+    .addFields(
+        {name: `Diretor Geral:`, value: ``}
+    )
+}
